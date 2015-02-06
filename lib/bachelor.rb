b82710d73f912efedeb1ef13d127dabe2441228a
axton21 <axton_n@yahoo.com>
2015-02-06 10:51:52 -0500

    TGIF

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,83 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data.each do |season_number, bachelorsarray|
+      #season = season number bachelorsarray = [{name => "beth"}]
+      bachelorsarray.each do |bachelor|
+        bachelor.each do |status, value|
+          #status = name, age  value = "beth, 26"
+          if season_number == season
+              name = value.split(" ")
+              return name[0]
+          # binding.pry
+          end
+        end
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.each do |season_number, bachelorsarray|
+      #season = season number bachelorsarray = [{name => "beth"}]
+      bachelorsarray.each do |bachelor|
+        bachelor.each do |status, value|
+          #status = name, age  value = "beth, 26"
+          if value == occupation
+              return bachelor["name"]
+            # binding.pry
+          end
+        end
+      end
+    end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+    count = 0
+    data.each do |season_number, bachelorsarray|
+        #season = season number bachelorsarray = [{name => "beth"}]
+      bachelorsarray.each do |bachelor|
+        bachelor.each do |status, value|
+            #status = name, age  value = "beth, 26"
+          if value == hometown
+                count += 1
+          end
+        end
+      end
+    end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, bachelorsarray|
+      #season = season number bachelorsarray = [{name => "beth"}]
+      bachelorsarray.each do |bachelor|
+        bachelor.each do |status, value|
+          #status = name, age  value = "beth, 26"
+          if value == hometown
+            return bachelor["occupation"]
+          end
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+    count = 0
+    age = 0
+    data.each do |season_number, bachelorsarray|
+        #season = season number bachelorsarray = [{name => "beth"}]
+        bachelorsarray.each do |bachelor|
+          bachelor.each do |status, value|
+            #status = name, age  value = "beth, 26"
+            if season_number == season
+                number = bachelor["age"]
+                count += 1
+                age += number.to_f
+                # binding.pry
+            end
+          end
+        end
+      end
+     average = age/count
+     # binding.pry
+     average.round
 end
